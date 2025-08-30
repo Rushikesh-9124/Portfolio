580b181d9fb0dfa659d0f522a40ad9cea0fd2335
Rushikesh-9124 <rushikesh6281@gmail.com>
2025-08-30 13:35:54 +0530

    Update Projects.jsx

--- frontend/src/pages/Projects/Projects.jsx
+++ frontend/src/pages/Projects/Projects.jsx
@@ -22,7 +22,7 @@ const Projects = () => {
   return (
     <div className="w-full h-full py-5  px-5 md:px-10 flex flex-col gap-5 overflow-auto">
       <h1 className="text-white text-xl md:text-2xl ml-1.5 font-medium ">Projects</h1>
-      <div className="grid grid-cols-1 max-sm:place-items-center max-sm:space-y-10 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 space-y-6 space-x-5 mb-2 max-md:mt-5 mt-2">
+      <div className="grid grid-cols-1 max-sm:place-items-center space-y-5 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 space-y-6 space-x-5 mb-2 max-md:mt-5 mt-2">
         {
           data?.map((item, idx)=>(
               <ProjectCard key={idx} data={item}/>
